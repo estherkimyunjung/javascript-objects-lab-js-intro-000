695b012b5eec30db10e66f95b116307b296955b0
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 23:16:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = {[key]: value};
   var result = Object.assign(object,newObj);
-  
-  if(object === newObj) {
-    newObj = {[key]: "undefined"};
-  } 
-    return object, result;
+  return result;
 }
