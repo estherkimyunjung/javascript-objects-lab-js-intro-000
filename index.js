6d7333be6eb413df67fcfc838afe2cd2b4bba82f
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 23:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  object = delete object.key;
+  delete object.key;
+  object = Object.assign({}, object);
   return object;
 }
